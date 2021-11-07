d60c006df1000da99663bb3d43ef7a3fd0d21ac2
fanshuaiko <1197303677@qq.com>
2021-11-07 15:55:20 +0800

    322题,计算并返回可以凑成总金额所需的最少的硬币个数。

---
+++ 动态规划/src/Number_322.java
@@ -0,0 +1,71 @@
+import java.util.Arrays;
+
+/**
+ * @ClassName Number_322
+ * @Description 给你一个整数数组 coins ，表示不同面额的硬币；以及一个整数 amount ，表示总金额。
+ * <p>
+ * 计算并返回可以凑成总金额所需的 最少的硬币个数 。如果没有任何一种硬币组合能组成总金额，返回 -1 。
+ * <p>
+ * 你可以认为每种硬币的数量是无限的。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：coins = [1, 2, 5], amount = 11
+ * 输出：3
+ * 解释：11 = 5 + 5 + 1
+ * 示例 2：
+ * <p>
+ * 输入：coins = [2], amount = 3
+ * 输出：-1
+ * 示例 3：
+ * <p>
+ * 输入：coins = [1], amount = 0
+ * 输出：0
+ * 示例 4：
+ * <p>
+ * 输入：coins = [1], amount = 1
+ * 输出：1
+ * 示例 5：
+ * <p>
+ * 输入：coins = [1], amount = 2
+ * 输出：2
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= coins.length <= 12
+ * 1 <= coins[i] <= 2^31 - 1
+ * 0 <= amount <= 10^4
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/coin-change
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author
+ * @CreateTime 2021/11/7 2:27 下午
+ * @Version 1.0
+ */
+public class Number_322 {
+    public static void main(String[] args) {
+
+        int[] coins = {1, 2, 5};
+        int amount = 3;
+        System.out.println(coinChange(coins, amount));
+    }
+
+    public static int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 0; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i - coin < 0) {
+                    continue;
+                }
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
+        }
+        return (dp[amount] == amount + 1) ? -1 : dp[amount];
+    }
+}
